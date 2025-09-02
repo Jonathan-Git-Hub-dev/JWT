508b87ab1ae69f83002048f6cbd600a34756ad54
Jonathan-Git-Hub-dev <Jonathan.d.pincus@gmail.com>
2025-09-02 13:57:42 +0800

    created config.php

---
+++ PHP/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = [
+    "database" => [
+        "server_name" => "",
+        "username" => "",
+        "password" => "",
+        "database" => "JWT"
+    ],
+    "mail_credentials" => [
+        "username" => "",
+        "password" => ""
+    ]
+
+];
